e5122d9da3b546b94e3f745bcd91b25b7476a5f1
techengine <24850353+tech-engine@users.noreply.github.com>
2024-09-10 15:11:11 +0530

    comment removed

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 func TestAdd(t *testing.T) {
 	result := Add(2, 3)
 	expected := 5
-	// this is a comment
+
 	if result != expected {
 		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
 	}
